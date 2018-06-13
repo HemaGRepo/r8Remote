9664c6c160f05c50e825f24d182e2ddd2b27b21e
Hema G <hema.g@capgemini.com>
2018-06-13 10:43:03 +0530

    V1.0

---
+++ App.java
@@ -0,0 +1,17 @@
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int i=0;
+	for(i=0;i<5;i++)
+	{
+        System.out.println( "Hello World!....");
+		System.out.println( ".............Jenkins Build..........");
+		System.out.println( "Success!...."+i);
+	}
+    }
+}
